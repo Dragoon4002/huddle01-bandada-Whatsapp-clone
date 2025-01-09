8bec85816351d08b85dfad274ce77c64da990efa
Dragoon4002 <sounak.dey2468@gmail.com>
2025-01-09 22:41:25 +0530

    Url matching working

---
+++ actions/getAccessToken.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { Role } from "@huddle01/server-sdk/auth";
+
+export const getAccessToken = async ({
+  roomId,
+  role,
+}: {
+  roomId: string;
+  role: Role;
+}) => {
+  const response = await fetch(
+    `http://localhost:3000/api/token?roomId=${roomId}&role=${role}`
+  );
+
+  const token = await response.text();
+  console.log(token);
+  
+  return token;
+};
